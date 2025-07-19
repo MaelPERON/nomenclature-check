2c66dea4d253fefb25543e13963dee75cffa4235
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-07-19 14:24:42 +0200

    add rules

---
+++ docs/rules.md
@@ -0,0 +1,22 @@
+| Type              | Prefix | Example                        | Versioned |
+|-------------------|--------|--------------------------------|-----------|
+| action           | ATN    | ATN_walk_cycle_v003           | True      |
+| armature         | ARM    | ARM_character_rig_v003        | True      |
+| environnement    | ENV    | ENV_montagne_modeling_v004    | True      |
+| fx               | FX     | None                          |           |
+| image            | IMG    | IMG_diffuse_brick_2k.jpg      |           |
+| lattice          | LAT    | LAT_facialDeform              |           |
+| library          | LIB    | LIB_matLib_shading            |           |
+| mesh             | MSH    | MSH_building_walls            |           |
+| mask             | MSK    | MSK_selection_hair            |           |
+| material         | MTL    | MTL_leather_worn              |           |
+| metarig          | MTA    | MTA_biped_standard            |           |
+| node_graph       | ND     | NOD_renderSetup               |           |
+| palette          | PLT    | PLT_character_colors          |           |
+| script           | SCT    | SCT_autoBake_v001.py          | True      |
+| simulation       | SIM    | None                          |           |
+| sound            | SND    | SND_ambient_forest_v001.wav   | True      |
+| shape_key        | SPK    | SPK_facial_expressions        |           |
+| text             | TXT    | TXT_credits_final_v001        | True      |
+| widget           | WGT    | WGT_faceControl               |           |
+| world            | WLD    | WLD_studio_lighting_v002      | True      |
---
+++ foreman.py
@@ -0,0 +1,89 @@
+RULES = {
+    "action": {
+        "prefix": "ATN",
+        "example": "ATN_walk_cycle_v003",
+        "versioned": True
+    },
+    "armature": {
+        "prefix": "ARM",
+        "example": "ARM_character_rig_v003",
+        "versioned": True
+    },
+    "environnement": {
+        "prefix": "ENV",
+        "example": "ENV_montagne_modeling_v004",
+        "versioned": True
+    },
+    "fx": {
+        "prefix": "FX",
+        "example": None
+    },
+    "image": {
+        "prefix": "IMG",
+        "example": "IMG_diffuse_brick_2k.jpg"
+    },
+    "lattice": {
+        "prefix": "LAT",
+        "example": "LAT_facialDeform"
+    },
+    "library": {
+        "prefix": "LIB",
+        "example": "LIB_matLib_shading"
+    },
+    "mesh": {
+        "prefix": "MSH",
+        "example": "MSH_building_walls"
+    },
+    "mask": {
+        "prefix": "MSK",
+        "example": "MSK_selection_hair"
+    },
+    "material": {
+        "prefix": "MTL",
+        "example": "MTL_leather_worn"
+    },
+    "metarig": {
+        "prefix": "MTA",
+        "example": "MTA_biped_standard"
+    },
+    "node_graph": {
+        "prefix": "ND",
+        "example": "NOD_renderSetup"
+    },
+    "palette": {
+        "prefix": "PLT",
+        "example": "PLT_character_colors"
+    },
+    "script": {
+        "prefix": "SCT",
+        "example": "SCT_autoBake_v001.py",
+        "versioned": True
+    },
+    "simulation": {
+        "prefix": "SIM",
+        "example": None
+    },
+    "sound": {
+        "prefix": "SND",
+        "example": "SND_ambient_forest_v001.wav",
+        "versioned": True
+    },
+    "shape_key": {
+        "prefix": "SPK",
+        "example": "SPK_facial_expressions"
+    },
+    "text": {
+        "prefix": "TXT",
+        "example": "TXT_credits_final_v001",
+        "versioned": True
+    },
+    "widget": {
+        "prefix": "WGT",
+        "example": "WGT_faceControl"
+    },
+    "world": {
+        "prefix": "WLD",
+        "example": "WLD_studio_lighting_v002",
+        "versioned": True
+    }
+}
