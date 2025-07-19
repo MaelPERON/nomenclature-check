2f18d6f08cd78ea3e0904deff4e7ea3df4a0ef13
MaelPERON <maelperonmessagerie2015@gmail.com>
2025-07-19 15:04:34 +0200

    standardize formatting and naming conventions in ASSET_RULES

--- foreman.py
+++ foreman.py
@@ -1,93 +1,93 @@
 import re
 
 ASSET_RULES = {
-    "action": {
-        "prefix": "ATN",
-        "example": "ATN_walk_cycle_v003",
-        "versioned": True
-    },
-    "armature": {
-        "prefix": "ARM",
-        "example": "ARM_character_rig_v003",
-        "versioned": True
-    },
-    "environnement": {
-        "prefix": "ENV",
-        "example": "ENV_montagne_modeling_v004",
-        "versioned": True
-    },
-    "fx": {
-        "prefix": "FX",
-        "example": None
-    },
-    "image": {
-        "prefix": "IMG",
-        "example": "IMG_diffuse_brick_2k.jpg"
-    },
-    "lattice": {
-        "prefix": "LAT",
-        "example": "LAT_facialDeform"
-    },
-    "library": {
-        "prefix": "LIB",
-        "example": "LIB_matLib_shading"
-    },
-    "mesh": {
-        "prefix": "MSH",
-        "example": "MSH_building_walls"
-    },
-    "mask": {
-        "prefix": "MSK",
-        "example": "MSK_selection_hair"
-    },
-    "material": {
-        "prefix": "MTL",
-        "example": "MTL_leather_worn"
-    },
-    "metarig": {
-        "prefix": "MTA",
-        "example": "MTA_biped_standard"
-    },
-    "node_graph": {
-        "prefix": "ND",
-        "example": "NOD_renderSetup"
-    },
-    "palette": {
-        "prefix": "PLT",
-        "example": "PLT_character_colors"
-    },
-    "script": {
-        "prefix": "SCT",
-        "example": "SCT_autoBake_v001.py",
-        "versioned": True
-    },
-    "simulation": {
-        "prefix": "SIM",
-        "example": None
-    },
-    "sound": {
-        "prefix": "SND",
-        "example": "SND_ambient_forest_v001.wav",
-        "versioned": True
-    },
-    "shape_key": {
-        "prefix": "SPK",
-        "example": "SPK_facial_expressions"
-    },
-    "text": {
-        "prefix": "TXT",
-        "example": "TXT_credits_final_v001",
-        "versioned": True
-    },
-    "widget": {
-        "prefix": "WGT",
-        "example": "WGT_faceControl"
-    },
-    "world": {
-        "prefix": "WLD",
-        "example": "WLD_studio_lighting_v002",
-        "versioned": True
-    }
+	"action": {
+		"prefix": "ATN",
+		"example": "ATN_walkCycle_v003",
+		"versioned": True
+	},
+	"armature": {
+		"prefix": "ARM",
+		"example": "ARM_characterRig_v003",
+		"versioned": True
+	},
+	"environnement": {
+		"prefix": "ENV",
+		"example": "ENV_montagneModeling_v004",
+		"versioned": True
+	},
+	"fx": {
+		"prefix": "FX",
+		"example": None
+	},
+	"image": {
+		"prefix": "IMG",
+		"example": "IMG_diffuseBrick2k.jpg"
+	},
+	"lattice": {
+		"prefix": "LAT",
+		"example": "LAT_facialDeform"
+	},
+	"library": {
+		"prefix": "LIB",
+		"example": "LIB_matLibShading"
+	},
+	"mesh": {
+		"prefix": "MSH",
+		"example": "MSH_buildingWalls"
+	},
+	"mask": {
+		"prefix": "MSK",
+		"example": "MSK_selectionHair"
+	},
+	"material": {
+		"prefix": "MTL",
+		"example": "MTL_leatherWorn"
+	},
+	"metarig": {
+		"prefix": "MTA",
+		"example": "MTA_bipedStandard"
+	},
+	"node_graph": {
+		"prefix": "ND",
+		"example": "NOD_renderSetup"
+	},
+	"palette": {
+		"prefix": "PLT",
+		"example": "PLT_characterColors"
+	},
+	"script": {
+		"prefix": "SCT",
+		"example": "SCT_autoBake_v001.py",
+		"versioned": True
+	},
+	"simulation": {
+		"prefix": "SIM",
+		"example": None
+	},
+	"sound": {
+		"prefix": "SND",
+		"example": "SND_ambientForest_v001.wav",
+		"versioned": True
+	},
+	"shape_key": {
+		"prefix": "SPK",
+		"example": "SPK_facialExpressions"
+	},
+	"text": {
+		"prefix": "TXT",
+		"example": "TXT_creditsFinal_v001",
+		"versioned": True
+	},
+	"widget": {
+		"prefix": "WGT",
+		"example": "WGT_faceControl"
+	},
+	"world": {
+		"prefix": "WLD",
+		"example": "WLD_studioLighting_v002",
+		"versioned": True
+	}
 }
 
 class Foreman:
